354da344ff01cc6955fd447cd4148f4460072f99
RazelRaz <razel16dcm@gmail.com>
2023-08-05 16:47:59 +0600

    Exploring Server Side Redering

--- app/product/page.js
+++ app/product/page.js
@@ -1,9 +1,23 @@
-import React from 'react';
+async function getData(){
+    let res = await fetch('https://dummyjson.com/products')
+    let json = await res.json()
+    return json['products']
+}
 
-const page = () => {
+const page = async () => {
+
+    let data = await getData()
+
     return (
         <div>
             <h1 className='my-color'>Product Page</h1>
+            {
+                data.map((item, key) => {
+                    return <div key={key}>
+                        <h2>{item['title']}</h2>
+                    </div>
+                })
+            }
         </div>
     );
 };
