ee0999924a5db1017ad39a2446bfbbfe90b0d24b
RazelRaz <razel16dcm@gmail.com>
2023-08-05 16:35:43 +0600

    Exploring Client Side Rendering

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+'use client'
+import React, { useEffect, useState } from 'react';
 
 const page = () => {
+
+  let [data, setData] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      let res = await fetch('https://dummyjson.com/products')
+      let json = await res.json()
+      setData(json['products']);
+    })()
+  },[])
+
+
   return (
     <div>
       <h1 className='my-color'>Home</h1>
+      {
+        data.map((item, index) => {
+          return <div key={index}>
+            <h2>{item['title']}</h2>
+          </div>
+        })
+      }
     </div>
   );
 };
--- app/product/page.js
+++ app/product/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            <h1>Product Page</h1>
+            <h1 className='my-color'>Product Page</h1>
         </div>
     );
 };
