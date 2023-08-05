ed2f8930e8c4f3d4866de55da9e3effd4daccf71
RazelRaz <razel16dcm@gmail.com>
2023-08-05 15:20:16 +0600

    Working With Global And Module CSS

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import styles from '../page.module.css'
 
 const page = () => {
     return (
         <div>
-            <h1>Contact Page</h1>
+            <h1 className={styles.textColor}>Contact Page</h1>
+            <p className={styles.myBackground}>Lorem ipsum dolor sit amet</p>
         </div>
     );
 };
