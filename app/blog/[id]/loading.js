31b657490510b17fd037d40fafe09223fe78dc7f
RazelRaz <razel16dcm@gmail.com>
2023-08-13 19:23:03 +0600

    added single blog page loading

---
+++ app/blog/[id]/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BloglLoading = () => {
+    return (
+        <div>
+            <h1>Loading.....! Single Blog page</h1>
+        </div>
+    );
+};
+
+export default BloglLoading;
