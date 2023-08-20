55f64fa5d09a6d1207b756912275f85b75879e63
RazelRaz <razel16dcm@gmail.com>
2023-08-20 15:29:39 +0600

    custom font family

--- app/layout.js
+++ app/layout.js
@@ -3,10 +3,18 @@
 import Menu from '@/components/Menu';
 import './globals.css'
 import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
+import {Oswald} from 'next/font/google'
+const OswaldFont = Oswald (
+  {
+    subsets:['cyrillic'],
+    weight: ['200','300','400','500','700'],
+    variable: '--font-OswaldFont'
+  }
+)
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={OswaldFont.variable}>
       <body>
         <Menu />
         {children}
