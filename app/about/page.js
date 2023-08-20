7f04e8197808eb6383a3b8013700838c0a2f4320
RazelRaz <razel16dcm@gmail.com>
2023-08-20 14:50:33 +0600

    image optimaization

--- app/about/page.js
+++ app/about/page.js
@@ -1,4 +1,4 @@
-"use client"
+import Image from "next/image";
 
 const AboutPage = () => {
     
@@ -6,7 +6,7 @@ const AboutPage = () => {
         <div>
             
             <h1>About Page</h1>
-            
+            <Image src='https://photo.teamrabbil.com/images/2023/08/15/banner.md.png'  alt="nature" width={500} height={300} />
         </div>
     );
 };
